5a1ca41ae22750ebecc2b59bb44c5a30e830cb7d
BhargavMN <mnbhargav8@gmail.com>
2023-01-25 23:08:52 +0100

    adding neurons, correcting MSE formula,log formatting

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import datetime
 
+
 #from sklearn.model_selection import train_test_split
 #log function to log the results for every time the model is run.
-def log(error,MSE,opt,n_epochs,n_batch):
+def log(error,MSE,opt,n_epochs,n_batch,n_layers,n_neurons):
     with open('logs.txt','a')as file:
-        file.write(f"{datetime.datetime.now()}- Optimizer: {opt}- Epoch number: {n_epochs:}- batch_size:{n_batch:}-MSE: {MSE:.4f}\n")
+        file.write(f"{datetime.datetime.now()} - Optimizer: {opt} - Number of layers: {n_layers} - Number of neuron: {n_neurons} - Epoch number: {n_epochs:} - batch_size:{n_batch:} - MSE: {MSE:.4f}\n")
 
 
 # Data set generation
@@ -26,14 +27,15 @@ y_learning, y_testing=noisey_y[:sample_length],noisey_y[sample_length:]
 #setting up the model. 
 opt='adam'
 los='mse'
+n_n=4
 model=tf.keras.Sequential()
-model.add(tf.keras.layers.Dense(2,input_shape=(1,), activation='relu'))
+model.add(tf.keras.layers.Dense(n_n,input_shape=(1,), activation='relu'))
 model.add(tf.keras.layers.Dense(1))
 model.compile(optimizer=opt, loss=los)
 
 #training the model. 
 n_epochs=450
-n_batch=32
+n_batch=35
 model.fit(x_learning,y_learning,epochs=n_epochs,batch_size=n_batch)
 
 #estimation
@@ -42,11 +44,11 @@ print(y_result)
 
 #checking for the accuracy of the model
 error=y_result-y_testing
-MSE=np.square(np.mean(error))
-print(error)
+MSE=np.mean(error**2)
+#print(error)
 print(MSE)
 
-log(error, MSE, opt, n_epochs, n_batch)
+log(error, MSE, opt, n_epochs, n_batch,2,n_n)
 
 
 #plotting 
